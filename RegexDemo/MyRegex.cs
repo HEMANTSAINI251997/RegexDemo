5a45275008e7bee89213e2226fbb0fdd5f5715d0
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-10 08:55:05 +0530

    [HEMANT]Add: DEMO of REGEX

---
+++ RegexDemo/MyRegex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class MyRegex
+    {
+        static string pattern = "^HEMANT$";
+        public void Check()
+        {
+            
+            Regex regex = new Regex(pattern);
+
+            if (regex.IsMatch("HEMANTa"))
+            { 
+                Console.WriteLine("String is match");
+            }
+            else
+            {
+                Console.WriteLine("String is not match");
+            }
+        }
+    }
+}
---
+++ RegexDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyRegex myregex = new MyRegex();
+            myregex.Check();
+
+        }
+    }
+}
